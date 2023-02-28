eb2bc45c60b4c2c2d97229e6e4d1b0268ebc80e1
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-02-27 19:31:00 -0500

    learning react, breakin' my backt

---
+++ RankingAppUsingReact/ClientApp/src/components/ItemCollection.js
@@ -0,0 +1,12 @@
+﻿import Item from "./Item";
+
+const ItemCollection = ({ items, drag, imgArr }) => {
+    <div className="items-not-ranked">
+        {
+            items.map((item) => (item.ranking === 0)
+                ? <Item item={item} drag={drag} itemImgObj={imgArr.find(o => o.id === item.imageId)} />
+                : null)
+        }
+    </div>
+}
+export default ItemCollection;
--- RankingAppUsingReact/ClientApp/src/components/RankItems.js
+++ RankingAppUsingReact/ClientApp/src/components/RankItems.js
@@ -1,6 +1,7 @@
 ﻿import React, { useState, useEffect } from 'react';
 import MovieImageArr from "./MovieImages.js";
 import RankingGrid from "./RankingGrid.js";
+import ItemCollection from "./ItemCollection.js";
 
 const RankItems = () => {
 
@@ -42,20 +43,8 @@ const RankItems = () => {
     return (
 
         <main>
-
             <RankingGrid items={items} imgArr={MovieImageArr} drag={drag} allowDrop={allowDrop} drop={drop} />
-
-            <div className="items-not-ranked">
-                {
-                    (items.length > 0) ? items.map((item) => 
-                        <div className="unranked-cell">
-                            <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image}
-                                style={{ cursor: "pointer" }} draggable="true" onDragStart={drag}
-                            />
-                        </div>
-                        ) : <div>Loading...</div>
-                }
-            </div>
+            <ItemCollection item={items} drag={drag} imgArr={MovieImageArr} />
         </main>
     )
 }
