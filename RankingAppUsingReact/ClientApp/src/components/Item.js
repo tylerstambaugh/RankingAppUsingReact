e4d66e80fb0ce0f46b5a3e92880725c7af5c3921
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-03-03 08:48:51 -0500

    can't get images to show :(

--- RankingAppUsingReact/ClientApp/src/components/Item.js
+++ RankingAppUsingReact/ClientApp/src/components/Item.js
@@ -2,13 +2,11 @@
 const Item = ({ item, drag, itemImgObj }) => {
     return (
         <div className="unranked-cell">
-            <img id={`item-${item.id}`} src={itemImgObj.image}
+            <img id={`item-${item.id}`} src={itemImgObj}
                 style={{ cursor: "pointer" }} draggable="true" onDragStart={drag} alt=""
             />
         </div>
 
     )
-
 }
-
 export default Item;
