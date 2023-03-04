9b8a04bdebb4736a9cd4c5a7e15452e173aac6eb
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-03-04 09:30:26 -0500

    starting on album images

---
+++ RankingAppUsingReact/ClientApp/src/components/AlbumImages.js
@@ -0,0 +1,26 @@
+import AbbeyRoad from '../images/albums/AbbeyRoad.png';
+import Adrenalize from '../images/albums/Adrenalize.png';
+import BackInBlack from '../images/albums/BackInBlack.png';
+import EnjoyTheSilence from '../images/albums/EnjoyTheSilence.png';
+import Parachutes from '../images/albums/Parachutes.png';
+import RideTheLightning from '../images/albums/RideTheLightning.png';
+import RockOrBust from '../images/albums/RockOrBust.png';
+import RustInPeace from '../images/albums/RustInPeace.png';
+import StAnger from '../images/albums/StAnger.png';
+import TheFinalCountDown from '../images/albums/TheFinalCountDown.png';
+
+const AlbumImageArr = [
+    { if: 1, image: AbbeyRoad },
+    { if: 2, image: Adrenalize },
+    { if: 3, image: BackInBlack },
+    { if: 4, image: EnjoyTheSilence },
+    { if: 5, image: Parachutes },
+    { if: 6, image: RideTheLightning },
+    { if: 7, image: RockOrBust },
+    { if: 8, image: RustInPeace },
+    { if: 9, image: StAnger },
+    { if: 10, image: TheFinalCountDown }
+
+];
+
+export default AlbumImageArr;
